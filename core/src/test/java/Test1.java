41ce9d9a130d136b3b1bdf422088040b28150f52
username <useremail>
2018-07-30 12:51:49 +0800

    添加一些github学习资料，很全面的知识

---
+++ core/src/test/java/Test1.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+
+/**
+ * Created by wang.honglin on 2018/7/30.
+ */
+public class Test1 {
+
+    @org.junit.Test
+    public void test() {
+
+    }
+
+    public static <T> T[] copy(T[] tClass){
+
+    }
+}
